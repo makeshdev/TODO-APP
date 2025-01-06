c973cc1f035ed24d499dcd5db125793e5f25d5ce
Makesh <makeshdev1995@gmail.com>
2025-01-06 08:11:02 +0530

    first commit

--- script.js
+++ script.js
@@ -1,164 +1,132 @@
-const form = document.getElementById("formbtn");
+let data = JSON.parse(localStorage.getItem("Data")) || [];
+
+let idCounter = data.length;
+let isEditMode = false;
+let currentEditId = null;
+
+let totalIncome = 0;
+let totalExpense = 0;
+
 const income = document.getElementById("income");
 const expense = document.getElementById("expense");
 const balance = document.getElementById("balance");
-let totalIncome = 0;
-let totalExpense = 0;
-let isEdit = false;
-let userData = null;
-form.onsubmit = async function (event) {
-  event.preventDefault();
-  try {
-    let title = document.getElementById("title").value;
-    let amount = document.getElementById("amount").value;
-    let opt = document.getElementById("opt").value;
-    if (isEdit == false) {
-      let url = await fetch(
-        "https://6773d58977a26d4701c67a4d.mockapi.io/api/v1/users",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            title: title,
-            amount: amount,
-            opt: opt,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      let data = await url.json();
-      form.reset();
-      getData();
-    } else {
-      await fetch(
-        `https://6773d58977a26d4701c67a4d.mockapi.io/api/v1/users/${userData}`,
-        {
-          method: "PUT",
-          body: JSON.stringify({
-            title: title,
-            amount: amount,
-            opt: opt,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      isEdit = false;
-      userData = null;
-      getData();
-      form.reset();
-    }
-    location.reload();
-  } catch (error) {
-    alert("somting went wrong" + error.message);
+
+//Read
+function readAll() {
+  localStorage.setItem("Data", JSON.stringify(data));
+  let item = localStorage.getItem("Data");
+  let objectdata = JSON.parse(item);
+
+  let ratioBtn = document.querySelectorAll('input[name="filter"]');
+  ratioBtn.forEach((radio) => {
+    radio.addEventListener("change", (event) => {
+      if (event.target.value === "income") {
+        const incomeData = data.filter((item) => item.opt === "Income");
+        showFun(incomeData);
+      } else if (event.target.value === "expense") {
+        const expenseData = data.filter((item) => item.opt === "Expense");
+        showFun(expenseData);
+      } else if (event.target.value === "all") {
+        showFun(data);
+      }
+    });
+  });
+
+  function showFun(filterData) {
+    let tableData = document.getElementById("tableData");
+    tableData.innerHTML = "";
+    filterData.forEach((element, id) => {
+      tableData.innerHTML += `<tr>
+                  <td class="pt-5">
+                  ${element.title}
+                  </td>
+                  <td class="pt-5">${element.amount}</td>
+                  <td class="pt-5">
+                    <span onclick=editData(${element.id})
+                      class="font-medium text-white bg-green-600 cursor-pointer px-3 py-1 rounded-md"
+                      >Edit</span
+                    >
+                    <span onclick=deleteData(${id})
+                      class="font-medium text-white bg-red-600 cursor-pointer px-3 py-1 rounded-md"
+                      >Delete</span
+                    >
+                  </td>
+                </tr>`;
+    });
   }
+  updateNum(data);
+  showFun(objectdata);
+}
+
+const form = document.getElementById("formbtn");
+//submit
+form.onsubmit = function create(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value.trim();
+  const amount = document.getElementById("amount").value.trim();
+  const opt = document.getElementById("opt").value;
+  if (title === "") {
+    alert("Please fill the details");
+    return false;
+  }
+  if (amount < 1 || amount === "") {
+    alert("Please enter a valid Amount!");
+    return false;
+  }
+  if (isEditMode) {
+    let conData = data.findIndex((index) => index.id === currentEditId);
+    if (conData !== -1) {
+      data[conData] = { id: currentEditId, title, amount, opt };
+    }
+  } else {
+    const NewData = {
+      id: ++idCounter,
+      title: title,
+      amount: amount,
+      opt: opt,
+    };
+    data.push(NewData);
+  }
+  localStorage.setItem("Data", JSON.stringify(data));
+  updateNum(data);
+  readAll();
+  form.reset();
 };
 
-async function getData() {
-  try {
-    let url = await fetch(
-      "https://6773d58977a26d4701c67a4d.mockapi.io/api/v1/users"
-    );
-    let data = await url.json();
-    data.forEach((ele) => {
-      if (ele.opt == "Income") {
-        totalIncome += parseInt(ele.amount);
-        income.innerText = "₹" + totalIncome;
-      } else {
-        totalExpense += parseInt(ele.amount);
-        expense.innerText = "₹" + totalExpense;
-      }
-      balance.innerText =
-        "₹" + (parseInt(totalIncome) - parseInt(totalExpense));
-    });
+//delete
+function deleteData(id) {
+  data.splice(id, 1);
 
-    let ratioBtn = document.querySelectorAll('input[name="filter"]');
-    ratioBtn.forEach((radio) => {
-      radio.addEventListener("change", (event) => {
-        if (event.target.value === "income") {
-          const incomeData = data.filter((item) => item.opt === "Income");
-          showFun(incomeData);
-        } else if (event.target.value === "expense") {
-          const expenseData = data.filter((item) => item.opt === "Expense");
-          showFun(expenseData);
-        } else if (event.target.value === "all") {
-          showFun(data);
-        }
-      });
-    });
-    function showFun(filterData) {
-      let tableData = document.getElementById("tableData");
-      tableData.innerHTML = "";
-      filterData.forEach((element) => {
-        tableData.innerHTML += `<tr>
-                    <td class="pt-5">
-                    ${element.title}
-                    </td>
-                    <td class="pt-5">${element.amount}</td>
-                    <td class="pt-5">
-                      <span onclick=editData(${element.id})
-                        class="font-medium text-white bg-green-600 cursor-pointer px-3 py-1 rounded-md"
-                        >Edit</span
-                      >
-                      <span onclick=deleteData(${element.id})
-                        class="font-medium text-white bg-red-600 cursor-pointer px-3 py-1 rounded-md"
-                        >Delete</span
-                      >
-                    </td>
-                  </tr>`;
-      });
+  updateNum(data);
+
+  readAll();
+}
+
+//edit
+function editData(id) {
+  let edit = data.find((res) => res.id === id);
+  document.getElementById("title").value = edit.title;
+  document.getElementById("amount").value = edit.amount;
+  document.getElementById("opt").value = edit.opt;
+  isEditMode = true;
+  currentEditId = id;
+  updateNum(data);
+}
+//Show Data
+function updateNum(data) {
+  totalIncome = 0;
+  totalExpense = 0;
+  data.forEach((ele) => {
+    if (ele.opt == "Income") {
+      totalIncome += parseInt(ele.amount);
+      income.innerText = "₹" + totalIncome;
+    } else {
+      totalExpense += parseInt(ele.amount);
+      expense.innerText = "₹" + totalExpense;
     }
-    showFun(data);
-  } catch (error) {
-    alert("somting wrong" + error.message);
-  }
+    balance.innerText = "₹" + (parseInt(totalIncome) - parseInt(totalExpense));
+  });
 }
-
-async function editData(params) {
-  try {
-    let url = await fetch(
-      `https://6773d58977a26d4701c67a4d.mockapi.io/api/v1/users/${params}`
-    );
-    let edit = await url.json();
-    document.getElementById("title").value = edit.title;
-    document.getElementById("amount").value = edit.amount;
-    document.getElementById("opt").value = edit.opt;
-    isEdit = true;
-    userData = params;
-  } catch (error) {
-    alert("somting went wrong" + error.message);
-  }
-}
-
-async function deleteData(params) {
-  try {
-    let conf = confirm("Are you sure want to delete?");
-    if (conf) {
-      let url = await fetch(
-        `https://6773d58977a26d4701c67a4d.mockapi.io/api/v1/users/${params}`,
-        {
-          method: "DELETE",
-        }
-      );
-      let data = await url.json();
-
-      if (data.opt == "Income") {
-        totalIncome -= parseInt(data.amount);
-        income.innerText = "₹" + totalIncome;
-      } else {
-        totalExpense -= parseInt(data.amount);
-        expense.innerText = "₹" + totalExpense;
-      }
-      balance.innerText =
-        "₹" + (parseInt(totalIncome) - parseInt(totalExpense));
-
-      getData();
-    }
-    location.reload();
-  } catch (error) {
-    alert("somting went wrong" + error.message);
-  }
-}
-getData();
+updateNum(data);
+readAll();
